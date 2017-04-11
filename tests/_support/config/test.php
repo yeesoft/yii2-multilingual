2593b64fef258cd09acb83982f31cd082102e734
YeeSoft <makitrataras@gmail.com>
2017-04-11 22:09:54 +0300

    updated .travis.yml

--- tests/_support/config/test.php
+++ tests/_support/config/test.php
@@ -8,9 +8,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=test',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=multilingual',
+            'username' => 'multilingual',
+            'password' => 'multilingual',
             'charset' => 'utf8',
         ],
         'mailer' => [
